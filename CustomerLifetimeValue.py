421ba243484a3cc6cb0460e30c529f300c06f656
monochromaticmonkey <83087571+monochromaticmonkey@users.noreply.github.com>
2022-08-09 17:37:46 -0400

    Update CustomerLifetimeValue.py
    Adding in more cost of goods sold information and adding in imports

--- CustomerLifetimeValue.py
+++ CustomerLifetimeValue.py
@@ -1,6 +1,8 @@
+import pandas as pd
+import numpy as np
+import random
+import PySimpleGUI as sg
 
-
-# The CLV class can call fuctions necessary for calculationg CLV and return CLV (w/h or w/h out Margin)
 class CustomerLifetimeValue:
     def __init__(self,revenue,num_purchases,num_unique_customers,sum_customer_lifespans,period_in_months,customers_begin_period,customers_end_period,inventory_beginning_period,inventory_end_period):
         self.revenue = revenue
@@ -26,27 +28,30 @@ class CustomerLifetimeValue:
             return 1
 
     def average_customer_lifespan(self):
-        #sum_customer_lifespans expressed in months - generally 12 or 24 months
-        ## your intial data should be pulled within a specific period as to avoid inaccurate values
+        #sum_customer_lifespans expressed in months
         return self.sum_customer_lifespans/self.num_unique_customers
 
     def churn_rate(self):
         #churn rate is the rate at which customers drop out of your customer pool
-        ## Not Actively Used in the following Script but Useful for Understand Your Customer Retention
         remaining_customers = self.customers_begin_period - self.customers_end_period
         return remaining_customers/self.customers_begin_period
 
 
     def cost_of_goods_sold(self):
-        ## THIS IS NOT DONE 
-        ### Comming Soon to a Script Near You
+        ### THIS IS NOT DONE###
         # refers to the direct cost of producting goods sold by the company. Includes matrerials and Labor cost used to create the goods and indirect expensesl like shipping and advertising.
-        return self.inventory_beginning_period-self.inventory_end_period
+        return (self.inventory_beginning_period + self.num_purchases)-self.inventory_end_period
 
     def gross_margin(self):
         pass
-        ### THIS IS NOT DONE ###
+        #(self.revenue - self.cost_of_goods_sold)/self.revenue)*100
+        
+        ### THIS IS NOT DONE###
         #(revenue - cost_of_goods_sold())/revenue
+    
+    def Average_Revenue_Per_User(self):
+        #revenue in a specific period
+        self.revenue/self.num_customers
 
     def customer_lifetime_value_wo_margin(self):
         #customer value during a year given a margin that you specify
@@ -56,3 +61,14 @@ class CustomerLifetimeValue:
         #customer value during a year given a margin that you specify
         margin = .30
         return self.average_purchase_value()*self.average_purchase_frequency()*self.average_customer_lifespan()*margin
+
+    def average_num_orders():
+        #define the perid and get the number of orders divided by transactions
+        return self.num_orders()/self.period_in_months
+    
+    def average_gross_margin():
+        #total gross margin percentaage per month
+        return (self.revenue - self.cost_of_goods_sold)/self.revenue
+    
+    def predictive_CLV_formula(self): 
+        self.revenue * (self.revenue/self.num_orders)
